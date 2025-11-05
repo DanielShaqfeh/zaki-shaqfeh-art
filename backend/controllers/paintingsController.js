82ff6c07c70b68a470dd9fc73ea0f8d9ad7e3aaf
Daniel Shaqfeh <danielshaqfeh@gmail.com>
2025-11-05 20:06:09 +0300

    fix painting controller and routes to work with cloudinary cloud storage

--- backend/controllers/paintingsController.js
+++ backend/controllers/paintingsController.js
@@ -16,6 +16,27 @@ export const getMostLikedPaintings = async (req, res) => {
   }
 };
 
+// Get top 3 most liked paintings (Homepage)
+export const getTopThreePaintings = async (req, res) => {
+  try {
+    const result = await pool.query(`
+      SELECT 
+        p.id, 
+        p.image_url, 
+        p.created_at, 
+        COUNT(l.id) AS likes
+      FROM paintings p
+      LEFT JOIN likes l ON p.id = l.painting_id
+      GROUP BY p.id
+      ORDER BY likes DESC, p.created_at DESC
+      LIMIT 3
+    `);
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
 // Get paintings liked by a specific user
 export const getUserLikedPaintings = async (req, res) => {
   const userId = req.params.userId;
--- backend/routes/paintingRoutes.js
+++ backend/routes/paintingRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getMostLikedPaintings, getUserLikedPaintings } from "../controllers/paintingsController.js";
+import { getMostLikedPaintings, getTopThreePaintings, getUserLikedPaintings } from "../controllers/paintingsController.js";
 
 const router = express.Router();
 
 router.get("/most-liked", getMostLikedPaintings);
+router.get("/featured", getTopThreePaintings);
 router.get("/user/:userId", getUserLikedPaintings);
 
 export default router;
