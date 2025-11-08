1101f3c318a44ff0fab9994dc6f6a9ca516dbfad
Daniel Shaqfeh <danielshaqfeh@gmail.com>
2025-11-08 15:04:28 +0300

    fix: use correct DATABASE_URL for PostgreSQL

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DB_URL,
   ssl: { rejectUnauthorized: false }, // required for Render PostgreSQL
 });
 
